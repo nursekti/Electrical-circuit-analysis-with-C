b0a6a5137c3bde1a567acec77b57877aad5aacc1
nursekti <59988090+nursekti@users.noreply.github.com>
2020-04-30 16:22:09 +0700

    Update visualisasi_grafik.py

--- visualisasi_grafik.py
+++ visualisasi_grafik.py
@@ -1,30 +1,30 @@
-import pandas as pd
+#import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-dfIC2 = pd.read_csv("dataIc2.csv")
-dfIL2 = pd.read_csv("datail2.csv")
-dfVC2 = pd.read_csv("dataVc2.csv")
-dfVL2 = pd.read_csv("dataVl2.csv")
-
-plt.plot(dfIC2['waktu'], dfIC2['nilai'])
+x,y=np.loadtxt("dataIc2.txt",unpack=True,delimiter=',')
+plt.plot(x,y)
 plt.title("Grafik nilai arus C2")
 plt.xlabel("waktu (s)")
 plt.ylabel("Arus (A)")
 plt.show()
 
-plt.plot(dfIL2['waktu'], dfIL2['nilai'])
+x,y = np.loadtxt("dataIl2.txt", unpack=True, delimiter=',')
+plt.plot(x,y)
 plt.title("Grafik nilai arus L2")
 plt.xlabel("waktu (s)")
 plt.ylabel("Arus (A)")
 plt.show()
 
-plt.plot(dfVC2['waktu'], dfVC2['nilai'])
+x,y = np.loadtxt("dataVc2.txt", unpack=True, delimiter=',')
+plt.plot(x,y)
 plt.title("Grafik nilai Tegangan C2")
 plt.xlabel("waktu (s)")
 plt.ylabel("Tegangan (V)")
 plt.show()
 
-plt.plot(dfVL2['waktu'], dfVL2['nilai'])
+x,y = np.loadtxt("dataVl2.txt", unpack=True, delimiter=',')
+plt.plot(x,y)
 plt.title("Grafik nilai Tegangan L2")
 plt.xlabel("waktu (s)")
 plt.ylabel("Tegangan (V)")
