208c82f0e8f22470066ab9e10658f1118291537f
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-30 14:19:08 +0700

    Delete oneper.c

--- oneper.c
+++
@@ -1,8 +0,0 @@
-compleks oneper(compleks x){
-    compleks z;
-    float pembagi;
-    pembagi = (x.real*x.real)+(x.imj*x.imj);
-    z.real = (x.real/pembagi);
-    z.imj =(-x.imj/pembagi);
-    return z;
-}
