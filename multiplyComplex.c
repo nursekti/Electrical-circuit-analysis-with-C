24d4e5cdc5922ddde5930dda04752d0482dbf142
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-30 14:23:40 +0700

    Create multiplyComplex.c

---
+++ multiplyComplex.c
@@ -0,0 +1,11 @@
+typedef struct{
+	float real;
+	float imj;
+} compleks;
+
+compleks multiplyComplex (compleks x, compleks y){
+	compleks z;
+	z.real = x.real*y.real - x.imj*y.imj;
+	z.imj = x.imj*y.real + x.real*y.imj;
+	return z;
+}
