79331247f37796ca5bf1447c08cc658433caa81d
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-30 14:25:41 +0700

    Create addComplex.c

---
+++ addComplex.c
@@ -0,0 +1,11 @@
+typedef struct{
+	float real;
+	float imj;
+} compleks;
+
+compleks addComplex (compleks x, compleks y){
+	compleks z;
+	z.real = x.real+y.real;
+	z.imj = x.imj+y.imj;
+	return z;
+}
